4f0a26bbd5fe89805849b3b1c5abb9daff4ba29f
José Miguel <jm@0pt1mates.com>
2022-05-30 18:44:25 +0200

    Better instructions and sum properties

---
+++ README.md
@@ -0,0 +1,20 @@
+# Exercise: sum numbers
+
+## Requisites
+
+- [nodejs](https://nodejs.org/dist/latest-v16.x/docs/api/)
+
+## Usage
+
+After cloning this repository run:
+
+```
+npm install
+```
+
+With all required node modules, run tests:
+
+```
+npm test
+```
+
--- sum.test.js
+++ sum.test.js
@@ -2,14 +2,26 @@ const sum = (a, b) => {
   return a + b;
 };
 
-it('2 + 3 equals 5', () => {
-  expect(sum(2, 3)).toBe(5);
+describe('sum 2 numbers', () => {
+  it('2 + 3 equals 5', () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+
+  it('12 + 3 equals 15', () => {
+    expect(sum(12, 3)).toBe(15);
+  });
 });
 
-it('12 + 3 equals 15', () => {
-  expect(sum(12, 3)).toBe(15);
+describe('commutative property', () => {
+  it('7 + 4 should be equal to 4 + 7', () => {
+    expect(sum(7, 4)).toBe(sum(4, 7));
+  });
 });
 
-it('7 + 4 should be equal to 4 + 7', () => {
-  expect(sum(7, 4)).toBe(sum(4, 7));
+describe('associative property', () => {
+  it('(7 + 2) + 1 equals to 7 + (2 + 1)', () => {
+    const actual = sum(sum(7, 2), 1);
+    const expected = sum(7, sum(2, 1));
+    expect(actual).toBe(expected);
+  });
 });
